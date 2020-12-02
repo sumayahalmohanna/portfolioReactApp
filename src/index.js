2d5721af496d0730920f5f5f0ee0a6910c9955f6
sumayahalmohanna <sumayahsaur@gmail.com>
2020-12-02 15:23:35 -0500

    Adjusting Routes

--- src/index.js
+++ src/index.js
@@ -81,7 +81,7 @@ const theme = createMuiTheme({
 });
 
 const app = (
-  <BrowserRouter>
+  <BrowserRouter basename='/portfolioReactApp'>
     <ThemeProvider theme={theme}>
         <App />
     </ThemeProvider>
