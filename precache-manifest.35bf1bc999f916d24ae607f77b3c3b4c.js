df8d76a27223d730512b6b2b374b6205f33d7aad
sumayahalmohanna <sumayahsaur@gmail.com>
2021-01-26 20:22:23 -0500

    Updates

---
+++ precache-manifest.35bf1bc999f916d24ae607f77b3c3b4c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb599f0013315fdf080cd66bb35d5a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecb7b0fcd80c8d033a2e",
+    "url": "/static/css/main.fd71e4c9.chunk.css"
+  },
+  {
+    "revision": "248137061c21db178f4d",
+    "url": "/static/js/2.939dd817.chunk.js"
+  },
+  {
+    "revision": "8192316cc1b6786565c9dca5c2c59331",
+    "url": "/static/js/2.939dd817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb7b0fcd80c8d033a2e",
+    "url": "/static/js/main.e175faad.chunk.js"
+  },
+  {
+    "revision": "93ccebd0741681011358",
+    "url": "/static/js/runtime-main.bbbe84dd.js"
+  },
+  {
+    "revision": "61b32a366e28ed88e445a673204832aa",
+    "url": "/static/media/Formik-Logo.61b32a36.png"
+  },
+  {
+    "revision": "cb2f65a2181811ba82d3db0ed0880d1c",
+    "url": "/static/media/MaterialUi-Logo.cb2f65a2.svg"
+  },
+  {
+    "revision": "4826d447d2b8018ab32b63baded56c59",
+    "url": "/static/media/React-Spring-Logo.4826d447.png"
+  },
+  {
+    "revision": "8e26f22094a11f6a689d8302dc30782c",
+    "url": "/static/media/ReactJs-Logo.8e26f220.svg"
+  },
+  {
+    "revision": "f9119f21225659f4e78364f003e6c10b",
+    "url": "/static/media/Step-Four.f9119f21.png"
+  },
+  {
+    "revision": "d10fa369e51bb9b44703d6cf26101220",
+    "url": "/static/media/Step-One.d10fa369.svg"
+  },
+  {
+    "revision": "248716e0cb31a611e0e6343f755b79be",
+    "url": "/static/media/Step-Three.248716e0.png"
+  },
+  {
+    "revision": "5c409d726a2b086e37ae573018190eb6",
+    "url": "/static/media/Step-Two.5c409d72.png"
+  },
+  {
+    "revision": "b3df3558067caa6eb442099e7e851bbb",
+    "url": "/static/media/designIcon.b3df3558.svg"
+  },
+  {
+    "revision": "f93625d1861f930f7ab2723a1b45ddfb",
+    "url": "/static/media/profilePic.f93625d1.svg"
+  },
+  {
+    "revision": "8ebd0b3f68ca0410b6a132e84df059d6",
+    "url": "/static/media/webdevIcon.8ebd0b3f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolioReactApp/precache-manifest.9f8870e42423e92864209945ace5d806.js"
+  "/precache-manifest.35bf1bc999f916d24ae607f77b3c3b4c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolioReactApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
